d047578bd7cd2f7018d59ebd9e8ad99ed298f878
KM.Kleczko <kklec001@stanford.edu>
2026-04-03 14:06:07 -0700

    Update rclone-automount.sh

--- rclone-automount.sh
+++ rclone-automount.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..5}; do \
-rclone mount FrydmanLabGDrive: ~/rClone/.rClone_FrydmanLabGDrive --vfs-cache-mode >= minimal && \
-rclone mount StanfordGDrive: ~/rClone/.rClone_StanfordGDrive --vfs-cache-mode >= minimal && \
+rclone mount FrydmanLabGDrive: ~/rClone/.rClone_FrydmanLabGDrive --vfs-cache-mode full --vfs-cache-max-size 25G && \
+rclone mount StanfordGDrive: ~/rClone/.rClone_StanfordGDrive --vfs-cache-mode full --vfs-cache-max-size 25G && \
 break || sleep 5; done
